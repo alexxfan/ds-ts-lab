a83eb648b3498ce881f3125cfa66c8879929c770
alexxfan <alexxfan>
2024-09-16 12:58:43 +0100

    Demo optional properties and parameters.

--- src/01-basics.ts
+++ src/01-basics.ts
@@ -1,16 +1,18 @@
 import {Friend, Colleague, ColleagueHistory } from './myTypes'
 
-const friend1 = {
-    name: "Jay Langford",
-    phone: "0863758394",
-    age: 21,
-};
-
-const friend2 = {
-    name: "Dylan Chai",
-    phone: "0834723648",
-    age: 21,
-};
+const friend1 : Friend = {
+    name: "Paul Fleming",
+    phone: "087-12345",
+    age: 25,
+    dob: new Date("1998-11-20") ,
+  };
+  
+  const friend2 : Friend = {
+    name: "Jane Costello",
+    phone: "086--12345",
+    age: 31,
+    interests: ['Music', 'Sport']
+  };
 
 export const friends = [friend1, friend2];
 // console.log(friends[1]);
